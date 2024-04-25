347e6ead27f2315e130af8b8769dd35242f04ff8
leoni <kylakov_leonid@mail.ru>
2024-04-25 13:20:01 +0300

    25.04.24

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        var bean = (HelloWorld) applicationContext.getBean("helloworld");
+        var bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1 == bean);
         System.out.println(bean.getMessage());
+
+        var bean2 = (Cat) applicationContext.getBean("cat");
+        var bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2 == bean3);
     }
 }
